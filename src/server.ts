13a3018f9cefbc9168fa83f06191261a45bd64fd
Opeyemi Adeniji <adenijiopeyemi68@gmail.com>
2022-09-09 16:21:10 +0100

    made changes

--- src/server.ts
+++ src/server.ts
@@ -50,7 +50,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
 
   // Start the Server
   app.listen( port, () => {
